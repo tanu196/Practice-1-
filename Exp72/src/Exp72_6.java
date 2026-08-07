93e4aa4076bd4b521cbdf201e5f43ddbdd615473
Iku <bsi26006@uhe.ac.jp>
2026-08-07 16:11:10 +0900

    twenty-five commit

--- Exp72/src/Exp72_6.java
+++ Exp72/src/Exp72_6.java
@@ -1,7 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Exp72_6 {
 	public static void main(String[] args) {
-		
+		Map<String , Integer> menuMap = new HashMap<>();
 		
 		
 		

591a68fe63331efa7c72275af592aa5f1f93d1a2
Iku <bsi26006@uhe.ac.jp>
2026-08-19 23:55:16 +0900

    thirty-four commit

---
+++ Exp90/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		Integer a = 100;
+		Integer b = 200;
+		
+		System.out.println(a.compareTo(a));
+		
+		
+	}
+
+}
---
+++ Exp90/src/Student.java
@@ -0,0 +1,27 @@
+
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	@Override
+	public int compareTo(Student o) {
+		// TODO 自動生成されたメソッド・スタブ
+		return 0;
+	}
+	
+	
+
+}

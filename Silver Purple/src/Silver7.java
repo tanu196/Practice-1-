aa6b16791c187f5463fab21c18da1d88a45a3142
Iku <bsi26006@uhe.ac.jp>
2026-09-06 00:50:10 +0900

    forty-five

--- Silver Purple/src/Silver7.java
+++ Silver Purple/src/Silver7.java
@@ -1,5 +1,16 @@
-public class Silver7 {
+record Item(int id , String name) {}
+
+class Silver7 {
 	public static void main(String[] args) {
+		Item item1 = new Item(100 , "T-shirts");
+		Item item2 = new Item(100 , "からあげ");
+		System.out.println(item1.id());
+		System.out.println(item1.name());
+		System.out.println(item1.equals(item2));
+		
+		System.out.println(item1.hashCode());
+		System.out.println(item1.toString());
+		System.out.println(item2.toString());
 		
 	}
 }

29ea0abb0dc0d614937cc4a9e0acdcc235f194e7
Iku <bsi26006@uhe.ac.jp>
2026-09-07 01:04:13 +0900

    forty-seven

---
+++ Silver Purple/src/Silver10.java
@@ -0,0 +1,9 @@
+public class Silver10{
+	public static void main(String[] args) {
+		int[] array = {1 ,2};
+		Object obj = array;
+		obj = 12;
+		obj = "nan";
+		
+	}
+}
---
+++ Silver/src/Silver11.java
@@ -0,0 +1,49 @@
+//class Man extends Woman{
+//	
+//}
+//class Woman{
+//	
+//}
+//
+//public class Silver11 {
+//	public static void main(String[] args) {
+//		Object obj = new Object();
+//		Man obj2 = new Man();
+//		if(!(obj instanceof Object s)) {
+////			System.out.println(s);
+//		}else {
+//			System.out.println(s);
+//			System.out.println("失敗");
+//		}
+//		
+//		if(!(obj2 instanceof Object s)) {
+////			System.out.println(s);
+//		}else if (!(obj2 instanceof  Woman n)){
+//			System.out.println(s);
+//			System.out.println("失敗");
+//		}else {
+//			System.out.println(n);
+//			
+//		}
+//		boolean n = false;
+//		if(!(obj2 instanceof Object s)) {
+////			System.out.println(s);
+//		}else if (n){
+//			System.out.println(s);
+//			System.out.println("失敗");
+//		}else {
+//			
+//		}
+//		
+//		
+//		if ( instanceof Object s) {}
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//		
+//	}
+//}

9bf3d0089780cf9ae90b1c74e32c7a5b269a7fd3
Iku <bsi26006@uhe.ac.jp>
2026-09-13 02:34:14 +0900

    fifty

---
+++ Silver17/src/Silver1.java
@@ -0,0 +1,24 @@
+public class Silver1 {
+	public static void main(String[] args) {
+		int nae = 13424;
+		int nje = 39421394;
+		
+		int n;
+		int j;
+		
+		int[][] num = new int[4][];
+		int[][][] num2 = new int[2][][];
+		String[][] s5 = {{},{}};
+		String[][] s6 = {{}};
+		String[][] s7 = {};
+		long num22 = 21421_4L;
+		
+		System.out.println("""
+				"nani"
+				""");
+	var nusdfff = new int[] {3,4,4321};
+	
+	
+	
+	}
+}
---
+++ Silver17/src/Silver2.java
@@ -0,0 +1,7 @@
+
+public class Silver2 {
+	public static void main(String[] args) {
+		
+		
+	}
+}

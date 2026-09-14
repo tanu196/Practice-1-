263a0176a1649ad439bfacbedf992a4ca9fc58da
Iku <bsi26006@uhe.ac.jp>
2026-09-14 12:01:48 +0900

    fifty-one

--- Silver17/src/Silver2.java
+++ Silver17/src/Silver2.java
@@ -1,6 +1,16 @@
-
 public class Silver2 {
 	public static void main(String[] args) {
+		int[] array = new int[4];
+		array[0] = 12;
+		array[1] = 123;
+		array[2] = 124;
+		array[3] = 126;
+		
+		
+		for(int arrays : array) {
+			System.out.println(arrays);
+			arrays++;
+		}
 		
 		
 	}
---
+++ Silver17/src/Silver3.java
@@ -0,0 +1,15 @@
+public class Silver3 {
+	public static void main(String[] args) {
+		final int num;
+		num = 23242;
+		
+		Test test = new Test();
+		
+		Test test2;
+		test2 = new Test();
+	}
+}
+
+class Test{
+	int num;
+}

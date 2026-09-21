02f8fc78e21235140c2bc6fd0c3d81370ec75c05
Iku <bsi26006@uhe.ac.jp>
2026-09-21 16:12:59 +0900

    fifty-six

--- Silver17/src/Silver9.java
+++ Silver17/src/Silver9.java
@@ -1,10 +1,27 @@
 public class Silver9 {
 	public static void main(String[] args) {
+		String num = "あしたこ";
+		num = "あしたわ";
+		System.out.println(num);
 		
 		
 		
+		String num2 = "あむ";
+		String num3 = "あむ";
+		
+		System.out.println(num2 == num3);
+		System.out.println(num2.equals(num3));
 		
 		
+		int[] n = new int[0];
+		String[] m = new String[100];
+		
+		m[0] = "dsfsdfdf";
+		System.out.println(m);
+		char[] af = new char[2];
+		af[0] = 'w';
+		af[1] = 'r';
+		System.out.println(af);
 		
 		
 	}
